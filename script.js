2061e4eba5c1da178992d59018232a4b6f62d4b9
Mateusz <wzmateusz@gmail.com>
2022-08-18 19:07:25 +0200

    exchange rate app html, css, js

---
+++ script.js
@@ -0,0 +1,35 @@
+const currencyOne = document.querySelector('#currency-one')
+const amountOne = document.querySelector('.amount-one')
+const currencyTwo = document.querySelector('#currency-two')
+const amountTwo = document.querySelector('.amount-two')
+const swapBtn = document.querySelector('.swap')
+const rateInfo = document.querySelector('.rate-info')
+
+const calculate = () => {
+	fetch(`https://api.exchangerate.host/latest?base=${currencyOne.value}&symbols=${currencyTwo.value}`)
+		.then(res => res.json())
+		.then(data => {
+			const currency1 = currencyOne.value // przechowują wartość pól
+			const currency2 = currencyTwo.value
+
+			const rate = data.rates[currency2] // przechowuje wartość jako aktualny kurs walut
+			rateInfo.textContent = `1 ${currency1} = ${rate.toFixed(4)} ${currency2}`
+
+			amountTwo.value = (amountOne.value * rate).toFixed(2)
+		})
+}
+
+// zamienia miejscami waluty
+const swap = () => {
+	const oldValue = currencyOne.value
+	currencyOne.value = currencyTwo.value
+	currencyTwo.value = oldValue
+	calculate()
+}
+
+currencyOne.addEventListener('change', calculate)
+currencyTwo.addEventListener('change', calculate)
+amountOne.addEventListener('input', calculate)
+swapBtn.addEventListener('click', swap)
+
+calculate()
